5d2acf28c6bff2ef2aff6843fd086ce916992116
edensongjbs <edensongjbs@gmail.com>
2020-09-18 12:50:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here!
 
-printString = (str) => {
+function printString(str) {
     console.log(str[0])
     if (str.length>1) {
         printString(str.slice(1,str.length))
@@ -9,3 +9,21 @@ printString = (str) => {
         return true
     }
 } 
+
+function reverseString(str) {
+    if (str.length>1) {
+        return str[str.length-1]+reverseString(str.slice(0,str.length-1))
+    }
+    else {
+        return str
+    }
+}
+
+function isPalindrome(str) {
+    if (str.length>2) {
+        return (str[0]===str[str.length-1]) && isPalindrome(str.slice(1,str.length-1))
+    }
+    else {
+        return (str[0]===str[str.length-1])
+    }
+}
